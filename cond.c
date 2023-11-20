28e8d7362bec2d813753f505a2e97287d000efe8
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 11:04:46 +0000

    Mon, Nov 20, 2023, 4:34 PM +05:30

--- cond.c
+++ cond.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main()
+int main(void)
 {
- 
-    int x=get_int("x value?\n");
-    int y=get_int("y value?\n");
+
+    int x= get_int("x value?\n");
+    int y= get_int("y value?\n");
     if (x>y)
     {
         printf(" x is greater than y");

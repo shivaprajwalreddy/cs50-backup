2f341da55261d4ace3484a38c364f191a6d5729d
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 13:59:23 +0000

    Mon, Nov 20, 2023, 7:29 PM +05:30

--- week1/function.c
+++ week1/function.c
@@ -3,20 +3,84 @@
 
 int main()
 {
-    int i,j,n;
-    do
-    {
-    n=get_int("size=");
-    }
-    while(n<1);
+    int get_size();
 
-    
-
+    int print();
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int get_size();
+{
  int i,j,n;
     do
     {
     n=get_int("size=");
     }
     while(n<1);
+}
+
+int print();
+{
+for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+        printf("#");
+        }
+        printf("\n");
+    }
+}

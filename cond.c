70050c54667c37ba67d598e0e2fbaf89c365e45e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 11:26:55 +0000

    Mon, Nov 20, 2023, 4:56 PM +05:30

--- cond.c
+++ cond.c
@@ -5,27 +5,32 @@ int main(void)
 {
     int q;
     int x,y;
+
     for(q=0;q<3;q++)
+
     {
     x= get_int("x value?\n");
     y= get_int("y value?\n");
 
-
-
     if (x>y)
+
     {
         printf(" x is greater than y\n");
 
     }
+
     else if(x<y)
+
     {
         printf("x is less than y\n");
 
     }
+
     else
     {
         printf("x is equal to y\n");
     }
+
     }
 
 }

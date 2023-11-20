3b515f53fedc84c6cb116c8adf3af635263696e0
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 11:09:23 +0000

    Mon, Nov 20, 2023, 4:39 PM +05:30

--- cond.c
+++ cond.c
@@ -9,5 +9,16 @@ int main(void)
     if (x>y)
     {
         printf(" x is greater than y\n");
+
     }
+    else if(x<y)
+    {
+        printf("x is less than y\n");
+
+    }
+    else
+    {
+        printf("x is equal to y");
+    }
+
 }

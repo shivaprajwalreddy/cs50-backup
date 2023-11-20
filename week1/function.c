f72f17c79fad08ad9dcb1f7c3d311cbf6d5a14e3
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 13:56:33 +0000

    Mon, Nov 20, 2023, 7:26 PM +05:30

--- week1/function.c
+++ week1/function.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main()
+{
+    int i,j,n;
+    do
+    {
+    n=get_int("size=");
+    }
+    while(n<1);
+
+    
+
+}
+
+ int i,j,n;
+    do
+    {
+    n=get_int("size=");
+    }
+    while(n<1);

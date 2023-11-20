08902aa6dff0173248cd0be6c8945fad12578b4b
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-20 11:00:34 +0000

    Mon, Nov 20, 2023, 4:30 PM +05:30

---
+++ cond.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int x,y;
+    int x=get_int("x value?\n");
+    int y=get_int("y value?\n");
+    if (x>y)
+    {
+        printf(" x is greater than y");
+    }
+}

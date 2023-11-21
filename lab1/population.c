ab9d47a86b37247cc196e191a44f4e728a1b7e4e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2023-11-21 13:31:33 +0000

    Tue, Nov 21, 2023, 7:01 PM +05:30

--- lab1/population.c
+++ lab1/population.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include <conio.h>
+#include <>
 
 int main()
 {
-    
+    int size;
+
+
+    do
+    {
+        size=get_int("population size:\n");
+    }
+    while(size>0)
 
 
 }

312a4eef40cde0057e0c139ed44cdd96672cb79e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-10 12:38:12 +0000

    Sat, Feb 10, 2024, 6:08 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,6 +1,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_row(int length);
+
 int main(void)
 {
     int n;
@@ -9,9 +11,13 @@ int main(void)
         n = get_int("Height: ");
     }
     while (n < 1);
+    print_row(n);
 }
 
 void print_row(int length)
 {
-    
+    for (int i=0; i<length; i++)
+    {
+        printf("#");
+    }
 }

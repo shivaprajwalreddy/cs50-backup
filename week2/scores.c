ec3b0663e87ec7832e90c4ebc674844bc05d18b8
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-15 15:32:53 +0000

    Thu, Feb 15, 2024, 9:02 PM +05:30

--- week2/scores.c
+++ week2/scores.c
@@ -1,1 +1,12 @@
-#include <
+#include <cs50.h>
+#include <stdio.h>
+
+int main()
+{
+    int scores1 = 72;
+    int scores2 = 73;
+    int scores3 = 33;
+
+    printf("avg=%i\n",(scores1 + scores2 + scores3)/3);
+}
+

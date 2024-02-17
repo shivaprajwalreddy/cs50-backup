52c3a0a887a7957aafa56a294694d3b29017141d
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-17 14:27:02 +0000

    Sat, Feb 17, 2024, 7:57 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,10 +3,19 @@
 #include <stdio.h>
 #include <string.h>
 
+int compute_score(string word);
+
 int main()
 {
     string player1 = get_string("player1: ");
     string player2 = get_string("player2: ");
 
+    int score1 = compute_score(player1);
+    int score2 = compute_score(player2);
+
+    if(score1 > score2)
+    {
+        printf("player1 wins)
+    }
 
 }

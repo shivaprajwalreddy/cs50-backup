05ac28bda00e91821795898be9e6626b596477a1
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-17 14:30:38 +0000

    Sat, Feb 17, 2024, 8:00 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,15 +7,27 @@ int compute_score(string word);
 
 int main()
 {
-    string player1 = get_string("player1: ");
-    string player2 = get_string("player2: ");
+    string player1 = get_string("player 1: ");
+    string player2 = get_string("player 2: ");
 
     int score1 = compute_score(player1);
     int score2 = compute_score(player2);
 
     if(score1 > score2)
     {
-        printf("player1 wins)
+        printf("player 1 wins!\n");
     }
+    if else(score2 > score1)
+    {
+        printf("player 2 wins!\n");
+    }
+    else
+    {
+        printf("tie!\n");
+    }
+}
 
+int compute_score(string word)
+{
+    
 }

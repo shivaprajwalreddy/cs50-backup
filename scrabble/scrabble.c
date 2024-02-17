c8f27a438d410afffd85c2487b4741f8db545131
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-17 14:34:55 +0000

    Sat, Feb 17, 2024, 8:04 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
 int compute_score(string word);
 
 int main()
@@ -29,5 +31,7 @@ int main()
 
 int compute_score(string word)
 {
+    int score = 0;
+
     
 }

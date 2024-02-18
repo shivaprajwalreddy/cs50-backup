82f0687c8e284eb3dac8f5e042973d227f1c6ae1
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-18 19:17:50 +0000

    Mon, Feb 19, 2024, 12:47 AM +05:30

---
+++ caesar/cae.c
@@ -0,0 +1,72 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+bool only_digits(string argv);
+char rotate(char p, int i);
+
+int main(int argc, string argv[])
+{
+    if (argc == 2)
+    {
+        bool digit = only_digits(argv[1]);
+        if (digit == false)
+        {
+            printf("usage: ./caesar key\n");
+            return 1;
+        }
+        else
+        {
+
+            return 0;
+        }
+        return 0;
+    }
+    else
+    {
+        printf("usage: ./caesar key\n");
+        return 1;
+    }
+    int key = atoi(argv[1]);
+            string text = get_string("plaintext:  ");
+            printf("ciphertext: ");
+            for (int i = 0, length = strlen(text); i < length; i++)
+            {
+                char c = rotate(text[i], key);
+                printf("%c", c);
+            }
+            printf("\n");
+}
+
+bool only_digits(string s)
+{
+    for (int i = 0, length = strlen(s); i < length; i++)
+    {
+        if (!isdigit(s[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+char rotate(char p, int i)
+{
+    char c;
+    if (isupper(p))
+    {
+        c = ((p - 'A') + i) % 26 + 'A';
+    }
+    else if (islower(p))
+    {
+        c = ((p - 'a') + i) % 26 + 'a';
+    }
+    else
+    {
+        c = p;
+    }
+    return c;
+}

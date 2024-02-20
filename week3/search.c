8df78a94ca33f165b46a09b76755df9d4bc24c5e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-20 16:04:15 +0000

    Tue, Feb 20, 2024, 9:34 PM +05:30

--- week3/search.c
+++ week3/search.c
@@ -3,5 +3,16 @@
 
 int main()
 {
-    int numbers[] = [10, 48, 86, 684, 61684, 6414, 69]
+    int numbers[] = [10, 48, 86, 684, 61684, 6414, 69];
+
+    int n = get_int("number: ");
+    for (i = 0; i < 7; i++)
+    {
+        if (numbers[i] == n)
+        {
+            printf("found\n");
+            return 0;
+        }
+    }
+    printf("not found\n");
 }

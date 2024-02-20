84453c1a670d4796c82572f98615c13e07f63c20
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-20 16:05:25 +0000

    Tue, Feb 20, 2024, 9:35 PM +05:30

--- week3/search.c
+++ week3/search.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-    int numbers[] = [10, 48, 86, 684, 61684, 6414, 69];
+    int numbers[] = {10, 48, 86, 684, 61684, 6414, 69};
 
     int n = get_int("number: ");
-    for (i = 0; i < 7; i++)
+    for (int i = 0; i < 7; i++)
     {
         if (numbers[i] == n)
         {

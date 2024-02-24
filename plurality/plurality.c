b394143f56e8d8b9fa81f95f160d369604df2fe3
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-02-24 18:16:22 +0000

    Sat, Feb 24, 2024, 11:46 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -70,7 +70,7 @@ bool vote(string name)
     {
         if (strcmp(name, candidate[i].name) == 0)
         {
-            candidate.votes[i]++;
+            candidate[i].votes++;
             return true;
         }
     }

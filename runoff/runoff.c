d77727c1c0d746cfa209156524bac663a5ed4a18
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-09 18:35:59 +0000

    Wed, Apr 10, 2024, 12:05 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -156,7 +156,7 @@ void tabulate(void)
         {
             if (preference[i][1] == 1)
             {
-                candidates[i].vote++;
+                candidates[i].votes++;
             }
         }
     }

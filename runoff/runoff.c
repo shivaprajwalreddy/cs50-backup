7b6d27c278cdf549a24360e094be386b3354a968
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-09 19:48:38 +0000

    Wed, Apr 10, 2024, 1:18 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -163,7 +163,7 @@ bool print_winner(void)
     {
         if (candidates[i].votes > (voter_count / 2) )
         {
-            printf("%s", candidates[i].name);
+            printf("%s\n", candidates[i].name);
             return true;
         }
     }

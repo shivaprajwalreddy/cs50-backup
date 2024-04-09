67e750a91a580a860f737e83a326456586fa52cf
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-09 19:26:02 +0000

    Wed, Apr 10, 2024, 12:56 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -193,7 +193,14 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes = min)
+            {
+                return true;
+            }
     return false;
 }
 

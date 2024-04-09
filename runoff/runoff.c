74d64b0520b1020285121711c1d717316f214426
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-09 19:19:46 +0000

    Wed, Apr 10, 2024, 12:49 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -177,7 +177,7 @@ int find_min(void)
     int min = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[preferences[i][j]].eliminated == false)
+        if (candidates[i].eliminated == false)
         {
             if (candidates[i].votes < min)
             {

54361daa95ee6f4a0b30eba9d6a8beff9c1680b1
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-22 19:33:22 +0000

    Tue, Apr 23, 2024, 1:03 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -51,7 +51,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            
+            image[i][j]
         }
     }
     return;

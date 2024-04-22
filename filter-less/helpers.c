673bba1b11641f908177be8331ec60d92d595993
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-22 18:30:46 +0000

    Tue, Apr 23, 2024, 12:00 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue / 3.0;
+            int average = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue / 3.0;
             
         }
     }

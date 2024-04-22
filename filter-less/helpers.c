8f524ce0f858b3e4f459ea189d216bef4de4e122
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-22 19:11:58 +0000

    Tue, Apr 23, 2024, 12:41 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -33,9 +33,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;
             sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue;
 
-            round(sepiaRed);
-            round(sepiaGreen);
-            round(sepiaBlue);
+            sepianRed = round(sepiaRed);
+            sepainGreen = round(sepiaGreen);
+            sepianBlue = round(sepiaBlue);
 
             if(sepiaRed > 255)
             {

bcb0357be6b66baa0f0472a552d1b20a2eefbaf7
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-22 18:29:27 +0000

    Mon, Apr 22, 2024, 11:59 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,6 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue / 3.0;
             
         }
     }

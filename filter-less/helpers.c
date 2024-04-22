e9786158524d0263f9791109e8148f374dba5396
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-22 19:51:23 +0000

    Tue, Apr 23, 2024, 1:21 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,9 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             float sepiaRed, sepiaGreen, sepiaBlue;
-            float originalred = image[i][j].rgbtRed;
-            float originalblue = image[i][j].rgbtBlue;
-            float originalgreen = image[i][j].rgbtGreen;
+            float originalRed = image[i][j].rgbtRed;
+            float originalBlue = image[i][j].rgbtBlue;
+            float originalGreen = image[i][j].rgbtGreen;
 
             sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue;
             sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue;

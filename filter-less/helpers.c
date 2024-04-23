e9c3c6a243f38177c1fa496cc8a0d44634db2127
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-23 11:24:29 +0000

    Tue, Apr 23, 2024, 4:54 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -19,7 +19,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 }
 
 // Convert image to sepia
-/*void sepia(int height, int width, RGBTRIPLE image[height][width])
+void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
@@ -44,7 +44,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         }
     }
     return;
-}*/
+}
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {

99ccc3794f3f4c24f0e15ef5e5123a5b76de4e9e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-24 19:24:47 +0000

    Thu, Apr 25, 2024, 12:54 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -91,6 +91,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             copy[i][j].rgbtRed = round(avgRed);
             copy[i][j].rgbtGreen = round(avgGreen);
             copy[i][j].rgbtBlue = round(avgBlue);
+
+            image[i][j] = copy[i][j];
         }
     }
 

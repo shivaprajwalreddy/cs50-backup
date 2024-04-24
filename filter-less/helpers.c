69e095633cf14362055698fa1d04068e9729b422
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-04-24 18:36:19 +0000

    Thu, Apr 25, 2024, 12:06 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -69,7 +69,10 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             copy[i][j] = image[i][j];
-            
+            for (int k = i - 1; k < 3; k++)
+            {
+                for (int l = )
+            }
         }
     }
 

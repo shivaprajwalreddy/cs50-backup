c57bb5a7489d121f39a1ee3235dd66ac7ad2191f
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-04 20:17:55 +0000

    Fri, Jul 5, 2024, 1:47 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[])
 {
+    int N = 512;
     // accept single line command line argument
     if (argc != 2)
     {
@@ -15,9 +16,10 @@ int main(int argc, char *argv[])
     FILE *card = fopen(argv[1], "r");
 
     // create a buffer
-    uint8_t buffer[512];
+    uint8_t buffer[N];
 
     // while data left to read
+    while (fread(buffer, 1, N, card) == N)
 
         // create JPEGs from data
 

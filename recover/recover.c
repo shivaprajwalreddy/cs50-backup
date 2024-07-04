8cb2cc5d52bfb5e3f4d88874f9b08040757651a9
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-04 20:03:11 +0000

    Fri, Jul 5, 2024, 1:33 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -4,8 +4,14 @@
 int main(int argc, char *argv[])
 {
     // accept single line command line argument
-    if (argc != 2
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+
     // open the memory card
+    FILE *card = fopen(argv[1], "r");
 
     // while data left to read
 

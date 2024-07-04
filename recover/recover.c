55ab78d26cd7f69ce40d16207263f9145a3967f9
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-04 19:57:52 +0000

    Fri, Jul 5, 2024, 1:27 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+    // accept single line command line argument
+    
+
+    // open the memory card
+
+    // while data left to read
+
+        // create JPEGs from data
+
 }

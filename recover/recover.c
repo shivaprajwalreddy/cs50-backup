00b4efab5c4e987cace465d293186cf6fa67d5b3
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-11 18:09:58 +0000

    Thu, Jul 11, 2024, 11:39 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -26,18 +26,6 @@ int main(int argc, char *argv[])
     {
         // Create JPEGs from the data
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-        {
-            sprintf(filename, "%03i.jpg", counter);
-            FILE *img = fopen(filename, "w");
-            fwrite(buffer, 1, N, filename);
-            counter++;
-        }
-        else
-        {
-            fclose(filename);
-            FILE *img = fopen(filename, "w");
-            fwrite(buffer, 1, N, filename);
-            counter++;
-        }
+    
     }
 }

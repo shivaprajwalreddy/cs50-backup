5a73cc46a06bcb6bb33faa5103d0bf5855203ca9
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-11 18:12:39 +0000

    Thu, Jul 11, 2024, 11:42 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -26,6 +26,39 @@ int main(int argc, char *argv[])
     {
         // Create JPEGs from the data
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-    
+         // If a JPEG is already open, close it
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            // Create a new JPEG file
+            sprintf(filename, "%03i.jpg", counter);
+            img = fopen(filename, "w");
+            if (img == NULL)
+            {
+                printf("Could not create output JPEG %s.\n", filename);
+                fclose(card);
+                return 1;
+            }
+
+            counter++;
+        }
+
+        // If a JPEG file is open, write the buffer to it
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, N, img);
+        }
+    }
+
+    // Close any remaining open files
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+    fclose(card);
+
+    return 0;
     }
 }

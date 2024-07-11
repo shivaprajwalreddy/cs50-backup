babeefa0b170a64ce748a0c0ba01901ef4b59aac
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-11 18:23:51 +0000

    Thu, Jul 11, 2024, 11:53 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[])
 {
     int N = 512;
+
     // Accept a single command-line argument
     if (argc != 2)
     {
@@ -14,20 +15,26 @@ int main(int argc, char *argv[])
 
     // Open the memory card
     FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("Could not open file %s.\n", argv[1]);
+        return 1;
+    }
 
     // Create a buffer for a block of data
     uint8_t buffer[N];
 
     char filename[8];
-    counter = 0;
+    int counter = 0;
     FILE *img = NULL;
 
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, N, card) == N)
     {
-        // Create JPEGs from the data
+        // Check for the beginning of a JPEG
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-         // If a JPEG is already open, close it
+        {
+            // If a JPEG is already open, close it
             if (img != NULL)
             {
                 fclose(img);
@@ -61,3 +68,4 @@ int main(int argc, char *argv[])
     fclose(card);
 
     return 0;
+}

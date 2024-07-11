079723e871344e003ecee12b29c51517d7dd692d
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-11 17:15:57 +0000

    Thu, Jul 11, 2024, 10:45 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -1,26 +1,7 @@
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 int main(int argc, char *argv[])
 {
-    int N = 512;
-    // accept single line command line argument
-    if (argc != 2)
-    {
-        printf("Usage: ./recover FILE\n");
-        return 1;
-    }
-
-    // open the memory card
-    FILE *card = fopen(argv[1], "r");
-
-    // create a buffer
-    uint8_t buffer[N];
-
-    // while data left to read
-    while (fread(buffer, 1, N, card) == N)
-
-        // create JPEGs from data
-
+ 
 }

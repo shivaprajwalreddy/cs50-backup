c8dcb0f2aa10ef2298a35ffbf1d014f1b70db61e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-11 17:54:51 +0000

    Thu, Jul 11, 2024, 11:24 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -20,15 +20,16 @@ int main(int argc, char *argv[])
 
     char filename[8];
     counter = 0;
-    
+
     // While there's still data left to read from the memory card
     while (fread(buffer, 1, N, card) == N)
     {
         // Create JPEGs from the data
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            sprintf(filename, "%03i.jpg", 0);
+            sprintf(filename, "%03i.jpg", counter);
             fwrite(buffer, 1, N, filename);
+            counter++;
         }
     }
 }

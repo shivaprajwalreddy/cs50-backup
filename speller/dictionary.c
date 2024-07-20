ef9bca988f8905693dcf9423f5772486973bdb86
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-20 21:15:11 +0000

    Sun, Jul 21, 2024, 2:45 AM +05:30

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -121,7 +121,7 @@ bool unload(void)
     // TODO
     for (int i = 0; i < N; i++)
     {
-        node *cursor = table[i]
+        node *cursor = table[i];
         while (cursor != NULL)
         {
             node *temp = cursor;

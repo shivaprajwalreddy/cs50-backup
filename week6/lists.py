b62fce4292d435a13a93596e5225a9149e9bbd06
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-22 21:17:37 +0000

    Tue, Jul 23, 2024, 2:47 AM +05:30

--- week6/lists.py
+++ week6/lists.py
@@ -1,7 +1,7 @@
 scores = []
 for i in range(3):
     score = int(input('score: '))
-    scores = scores.append(score)
+    scores.append(score)
 
 avg = sum(scores) / len(scores)
 print(f'average: {avg}')

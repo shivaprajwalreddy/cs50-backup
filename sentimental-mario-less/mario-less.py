3512a9787f3a8f0e5184d75a96b88e2fcb7d0d5e
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-23 21:04:56 +0000

    Wed, Jul 24, 2024, 2:34 AM +05:30

--- sentimental-mario-less/mario-less.py
+++ sentimental-mario-less/mario-less.py
@@ -1,10 +1,11 @@
 from cs50 import get_int
 
-while True:
-    n = get_int('height: ')
-    if n > 0 and n < 9:
-        break
 def main():
+    while True:
+        n = get_int('height: ')
+        if n > 0 and n < 9:
+            break
+
     for i in range(n):
         for j in range(n - 1):
             print(' ', end='')

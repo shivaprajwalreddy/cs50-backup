df01c72c1e5496cb95eb59e253eb40687cf08425
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-23 21:02:31 +0000

    Wed, Jul 24, 2024, 2:32 AM +05:30

--- sentimental-mario-less/mario-less.py
+++ sentimental-mario-less/mario-less.py
@@ -4,18 +4,18 @@ while True:
     n = get_int('height: ')
     if n > 0 and n < 9:
         break
-main()
-for i in range(n):
-    for j in range(n - 1):
-        print(' ', end='')
-    row(n)
-    print()
-    n -= 1
+def main():
+    for i in range(n):
+        for j in range(n - 1):
+            print(' ', end='')
+        row(n)
+        print()
+        n -= 1
 
-row(n)
-for i in range(n):
-    for j in range(n):
-        print('#')
-    n += 1
+def row(n):
+    for i in range(n):
+        for j in range(n):
+            print('#')
+        n += 1
 
 main()

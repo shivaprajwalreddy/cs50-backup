9f8f369e8684dffbba53c68eee4ca28cf8924fb0
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-25 18:35:27 +0000

    Fri, Jul 26, 2024, 12:05 AM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -5,8 +5,28 @@ while True:
     if change > 0:
         break
 
-quaters = 
+quaters = 0
+if change >= 25:
+    quaters += 1
+    change -= 25
 
-print(f'q: {quaters}')
+dime = 0
+if change >= 10:
+    dime += 1
+    change -= 10
+
+nickel = 0
+if change >= 5:
+    nickel += 1
+    change -= 5
+
+penny = 0
+if change >= 1:
+    penny += 1
+    change -= 1
+
+total = quaters + dime + nickel + penny
+
+print(f'{total}')
 
 

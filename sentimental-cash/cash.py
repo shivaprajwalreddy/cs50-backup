876bba6196426a4a202a4af009cd9d8b180126bd
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-25 18:40:44 +0000

    Fri, Jul 26, 2024, 12:10 AM +05:30

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -4,26 +4,27 @@ while True:
     change = get_float('change: ')
     if change > 0:
         break
+change = change * 100
 
 quaters = 0
-if change >= 0.25:
+if change >= 25:
     quaters += 1
-    change -= 0.25
+    change -= 25
 
 dime = 0
-if change >= 0.10:
+if change >= 10:
     dime += 1
-    change -= 0.10
+    change -= 10
 
 nickel = 0
-if change >= 0.5:
+if change >= 5:
     nickel += 1
-    change -= 0.5
+    change -= 5
 
 penny = 0
-if change >= 0.1:
+if change >= 1:
     penny += 1
-    change -= 0.1
+    change -= 1
 
 total = quaters + dime + nickel + penny
 

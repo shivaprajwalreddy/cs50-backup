e42e83fe0693edb01673ce01d4ccfd106c72cc35
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-07-26 19:33:29 +0000

    Sat, Jul 27, 2024, 1:03 AM +05:30

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,7 +4,7 @@ text = get_string('text: ')
 
 letters = sum(1 for char in text if char.isalpha())
 
-word = text.split()
-words = len(word)
+words = text.split()
+num_words = len(word)
 
 print(f'{words}')

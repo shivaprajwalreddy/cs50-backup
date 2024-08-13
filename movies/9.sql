7d7beafb2db30e736d906ad9cd1897ebec932fd6
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-08-13 07:38:32 +0000

    Tue, Aug 13, 2024, 1:08 PM +05:30

--- movies/9.sql
+++ movies/9.sql
@@ -1,7 +1,7 @@
-select name from people where id in
+select distinct name from people where id in
 (
     select person_id from stars where movie_id in
     (
         select id from movies where year = 2004
     )
-)
+)order by birth;

0d629142a2e14b93d9102327e05627a67ec374ba
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-08-13 11:33:05 +0000

    Tue, Aug 13, 2024, 5:03 PM +05:30

--- movies/10.sql
+++ movies/10.sql
@@ -2,6 +2,6 @@ select distinct name from people where id in
 (
     select person_id from directors where movie_id in
     (
-        select movie_id from ratings where ratings > 9.0
+        select movie_id from ratings where rating >= 9.0
     )
 );

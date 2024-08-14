6ea9feadda2b1180051779225cf99f3ffaca13ad
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-08-14 13:07:39 +0000

    Wed, Aug 14, 2024, 6:37 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -16,3 +16,10 @@ select * from flights where year = 2023 and month = 7 and day = 29 and origin_ai
  select * from phone_calls where year = 2023 and month = 7 and day = 28 and duration < 60;
 
  select * from bakery_security_logs where year = 2023 and month = 7 and day = 28 and hour = 10 and minute > 15 and minute < 25;
+
+ select * from people where phone_number in (select caller from phone_calls where year = 2023 and month = 7 and day = 28 and duration < 60)
+    and passport_number in (select passport_number from passengers where flight_id = 36)
+    and license_plate in (select license_plate from bakery_security_logs where year = 2023 and month = 7 and day = 28 and hour = 10 and minute > 15 and minute < 25)
+    and id in (select person_id from bank_accounts where account_number in (select account_number from atm_transactions where year = 2023 and month = 7 and day = 28 and atm_location = 'Leggett Street'));
+
+sqlite> select * from people where phone_number = '(375) 555-8161';

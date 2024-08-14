6883891bd3da8ebb9e41ec4a36d8215b9b94ee5f
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2024-08-14 13:03:44 +0000

    Wed, Aug 14, 2024, 6:33 PM +05:30

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,7 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+select * from crime_scene_reports where description like '%Duck%';
+
+select * from interviews where transcript like '%bakery%';
+
+select * from atm_transactions where year = 2023 and month = 7 and day = 28 and atm_location = 'Leggett Street';

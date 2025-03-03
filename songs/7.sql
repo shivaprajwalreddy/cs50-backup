58ff908ecb1e4efcbe68ef878b7be6db4f97c98b
shivaprajwalreddy <147982324+shivaprajwalreddy@users.noreply.github.com>
2025-03-03 22:46:42 +0530

    Organized SQL files into SQL directory

--- songs/7.sql
+++
@@ -1,8 +0,0 @@
-select avg(energy)
-from songs
-where artist_id =
-(
-    select id
-    from artists
-    where name = 'Drake'
-);
--- songs/8.sql
+++
@@ -1,3 +0,0 @@
-select name
-from songs
-where name like '%feat.%';
